726eb46077f2bdd285feec41ebce1a6a892364b3
Muhammad Areeb <muhammadareebkmh@gmail.com>
2024-12-17 22:44:26 +0500

    This is some after updates

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ function getTask(){
     let x = document.getElementById("taskText").value;
     let y = document.getElementById("taskDate").value;
     
-    document.getElementById("TaskPreviw").innerHTML = "Task: " + x + " Date: " + y;
+    localStorage.setItem("Task", x)
+    localStorage.setItem("Date", y)
+    let localTask = localStorage.getItem("Task")
+    let localDate = localStorage.getItem("Date")
+    document.getElementById("TaskPreviw").innerHTML = "Task: " + localTask + "Date: " + localDate;
+    if ((localTask && localDate) === (x && y)) {
+        document.taskData.reset();
+    }
 }
